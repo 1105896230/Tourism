44f37f326b90b1cd59f3504d43823b67032181dd
FMT <FMT157@126.com>
2016-03-12 13:13:38 +0800

    开始写旅游类型的DemoApp 引入主流的框架

---
+++ app/src/main/java/tourism/fmt/com/tourism/ApplicationConstant.java
@@ -0,0 +1,14 @@
+package tourism.fmt.com.tourism;/**
+ * Created by hasee-pc on 2016/3/12.
+ */
+
+/**
+ * User:FMT
+ * Email:1105896230@qq.com
+ * DATA:2016/3/12
+ * Time:12:53 
+ *
+ */
+public class ApplicationConstant {
+    public static final String APPKEY = "de739b335ce00e2287a9f2bee1690e62";
+}
